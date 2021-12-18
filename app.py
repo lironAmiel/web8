403263348e38c489f55925c50d85bb5cf4be1e9b
lironAmiel <92858697+lironAmiel@users.noreply.github.com>
2021-12-18 14:03:02 +0200

    add to remote

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask , render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def assignment8():  # put application's code here
+    return render_template('assignment8.html',
+                           myName ={'fristName' :"liron", 'lastName' :"amiel"},
+                            myLovedNumber =6,
+                           user= ['user1','user2'])
+
+
+if __name__ == '__main__':
+    app.run()
