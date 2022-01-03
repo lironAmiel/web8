da15456445bf23829b32c35a75073243436cfb80
lironAmiel <92858697+lironAmiel@users.noreply.github.com>
2022-01-03 11:37:22 +0200

    ass10

--- app.py
+++ app.py
@@ -1,5 +1,21 @@
 from flask import Flask , render_template
-from flask import request
+from flask import request, redirect
+import mysql.connector
+from interact_with_DB import interact_db
+
+'''mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="root",
+  database="users"
+)
+
+mycursor = mydb.cursor()
+
+sql = "INSERT INTO user (userNum, userName, email) VALUES (%s, %s, %s)"
+val = ("user1", "Liron", "amiel@gmail.com")
+mycursor.execute(sql, val)
+mydb.commit()'''
 
 
 app = Flask(__name__)
@@ -53,6 +69,41 @@ def login_func():
         username = request.form['username']
         return render_template('login.html',username=username)
 
+@app.route('/assigment10', methods=['GET','POST'])
+def ass10():
+    return render_template('assigment10.html')
+
+@app.route('/users', methods=['GET','POST'])
+def users():
+    query= 'select * from users;'
+    users= interact_db(query=query, query_type='fetch')
+    return render_template('users.html', users=users)
+
+@app.route('/insertUser', methods=['POST'])
+def insertUser():
+    name = request.form['name']
+    email = request.form['email']
+    password = request.form['password']
+    query = "insert into users(name, email, password) values ('%s','%s','%s')" % (name, email, password)
+    interact_db(query=query, query_type='commit')
+    return redirect('/users')
+
+@app.route('/delet_user', methods = ['POST'])
+def delet_user():
+    userId = request.form['id']
+    query = "delete from users where id='%s';" % userId
+    interact_db(query=query, query_type='commit')
+    return redirect('/users')
+
+@app.route('/update', methods = ['POST'])
+def update():
+    newName = request.form['newName']
+    name = request.form['name']
+    query = "update users set name= '%s' where name='%s';" % (newName, name)
+    interact_db(query=query, query_type='commit')
+    return redirect('/users')
+
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=true)
